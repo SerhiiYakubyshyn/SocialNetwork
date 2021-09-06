029695b5de7986e4e50d24b9be314192ce84e4bc
SerhiiYakubyshyn <70217790+SerhiiYakubyshyn@users.noreply.github.com>
2021-09-06 22:33:38 +0300

    Add files via upload

---
+++ Social_Network/Hubs/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Send", message, userName);
+        }
+
+
+        //public async Task SendTo(string message, string to)
+        //{
+        //    var userName = Context.User.Identity.Name;
+
+        //    if(Context.UserIdentifier != to)
+        //    {
+        //        await this.Clients.User(Context.UserIdentifier).SendAsync("SendTo", message, "MY");
+        //    }
+        //    await this.Clients.User(to).SendAsync("SendTo", message, userName);
+
+        //}
+
+    }
+}
