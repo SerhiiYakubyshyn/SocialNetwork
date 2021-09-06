74616ca7e48328742dafafa5f590c783d7bc6eb5
SerhiiYakubyshyn <70217790+SerhiiYakubyshyn@users.noreply.github.com>
2021-09-06 22:35:56 +0300

    Add files via upload

---
+++ Social_Network/AppBuilders/UseCulture.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.AppBuilders
+{
+    public static class UseCulture
+    {
+        public static IApplicationBuilder Culture(this IApplicationBuilder app)
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            return app;
+        }
+    }
+}
