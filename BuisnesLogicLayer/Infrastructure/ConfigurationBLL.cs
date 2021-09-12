e5b47abc26bcfba51d2ec3964e42aeaa74feda91
SerhiiYakubyshyn <70217790+SerhiiYakubyshyn@users.noreply.github.com>
2021-09-12 12:53:01 +0300

    Add files via upload

--- BuisnesLogicLayer/Infrastructure/ConfigurationBLL.cs
+++ BuisnesLogicLayer/Infrastructure/ConfigurationBLL.cs
@@ -12,25 +12,8 @@ using System.Threading.Tasks;
 
 namespace BuisnesLogicLayer.Infrastructure
 {
-    public static class ConfigurationBLL //: ISocialNetworkConfiguration
+    public static class ConfigurationBLL
     {
-        //IConfiguration configuration;
-        //public ConfigurationBLL(IConfiguration configuration)
-        //{
-        //    this.configuration = configuration;
-        //}
-        //public string SocialNetworkConnection => configuration["ConnectionStrings:DefaultConnection"];
-
-        //public string GetConnectionString(string connectionString)
-        //{
-        //    return configuration.GetConnectionString(connectionString);
-        //}
-        //public static void ConfigureServices(IConfiguration configuration)
-        //{
-
-        //    //services.AddDbContext<SocialNetworkContext>(opt => opt.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
-        //    //services.AddDbContext<SocialNetworkContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-        //}
 
         public static IServiceCollection AddDependecy(IServiceCollection services, IConfiguration configuration)
         {
@@ -44,18 +27,5 @@ namespace BuisnesLogicLayer.Infrastructure
 
             return services;
         }
-
-        //public IServiceCollection MyConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddSingleton(typeof(DataLayer.Interfaces.IWorkGroup), typeof(DataLayer.WorkTemp.WorkGroup));
-        //    services.AddSingleton(typeof(DataLayer.Interfaces.IWorkMessage), typeof(DataLayer.WorkTemp.WorkMessage));
-        //    services.AddSingleton(typeof(DataLayer.Interfaces.IWorkPost), typeof(DataLayer.WorkTemp.WorkPost));
-        //    services.AddSingleton(typeof(DataLayer.Interfaces.IWorkPrivateMessage), typeof(DataLayer.WorkTemp.WorkPrivateMessage));
-        //    services.AddSingleton(typeof(DataLayer.Interfaces.IWorkUserProfile), typeof(DataLayer.WorkTemp.WorkUserProfiles));
-        //    services.AddDbContext<DataLayer.Data.SocialNetworkContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            
-        //    //services.AddIdentityCore<DataLayer.Models.UserProfiles, IdentityRole>().AddEntityFrameworkStores<DataLayer.Data.SocialNetworkContext>();
-        //    return services;
-        //}
     }
 }
